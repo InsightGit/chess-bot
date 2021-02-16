2ce91e28bff0c084702601d9597e9cb15b73d1a7
Bobby Youstra <bobbyyoustra@protonmail.com>
2021-02-16 15:49:09 -0800

    Initial Commit

---
+++ src/chess/board.cpp
@@ -0,0 +1,6 @@
+//
+// Created by bobby on 16/02/2021.
+//
+
+#include "board.hpp"
+
---
+++ src/chess/board.hpp
@@ -0,0 +1,53 @@
+//
+// Created by bobby on 16/02/2021.
+//
+
+#ifndef CHESS_BOT_BOARD_HPP
+#define CHESS_BOT_BOARD_HPP
+
+#include <exception>
+#include <vector>
+
+#include "vector_2.hpp"
+
+namespace chess {
+    enum class PieceType {
+        King,
+        Queen,
+        Rook,
+        Bishop,
+        Knight,
+        Pawn
+    };
+
+    enum class GameOutcome {
+        BlackVictory,
+        Tie,
+        WhiteVictory,
+        InProgress
+    };
+
+    class InvalidMoveException : public std::exception {
+    public:
+        virtual const char *what() const throw() {
+            return "Invalid move attempted";
+        }
+    };
+
+    class Board {
+    public:
+        virtual std::vector<Board> possible_states() = 0;
+
+        // returns PieceType that was attacked, piece_id is for pieces like pawns that could have multiple pieces on the board
+        virtual PieceType attack(PieceType from_piece_type, PieceType to_piece_type, int from_piece_id = 0,
+                                 int to_piece_id = 0) = 0;
+        virtual PieceType attack(Vector2 from_pos, Vector2 to_pos) = 0;
+
+        virtual GameOutcome gameEndStatus() = 0;
+
+        virtual void move(Vector2 from_pos, Vector2 to_pos) = 0;
+        virtual void move(PieceType piece_type, Vector2 to_pos, int piece_id = 0) = 0;
+    };
+}
+
+#endif //CHESS_BOT_BOARD_HPP
