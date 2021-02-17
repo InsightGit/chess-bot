0ce6873506f80906e6cb385a672bbaa06bf03f6b
InterestingBrainPoops <satasiyahet@gmail.com>
2021-02-16 21:39:48 -0800

    Added move ordering
    Also moved to for each loops.

--- src/chess/minmax.hpp
+++ src/chess/minmax.hpp
@@ -4,10 +4,11 @@
 #include "board.hpp"
 #include <algorithm>
 #include <limits>
+#include <vector>
 namespace minimax{
     // maximizingplayer can be true or false, true being white, false being black.
     int minimax(chess::Board position, int depth, int alpha, int beta, bool maximizingplayer){
-        if(depth == 0 || position.gameEndStatus() != chess::GameOutcome::InProgress){
+        if(depth == 0 || position.game_end_status() != chess::GameOutcome::InProgress){
             if(maximizingplayer && chess::GameOutcome::WhiteVictory == position.gameEndStatus()){
                 return std::numeric_limits<int>::max();
             }else if(!maximizingplayer && chess::GameOutcome::BlackVictory == position.gameEndStatus()){
@@ -16,16 +17,17 @@ namespace minimax{
                 return std::numeric_limits<int>::min();
             }else if(maximizingplayer && chess::GameOutcome::BlackVictory == position.gameEndStatus()){
                 return std::numeric_limits<int>::min();
-            }else if(chess::GameOutcome::Tie == position.gameEndStatus()){
+            }else if(chess::GameOutcome::Tie == position.game_end_status()){
                 return 0;
             }
-            return position.getStaticEval();
+            return position.get_static_eval();
         }
         int eval;
+        std::vector<chess::Board> orderedMoves = order_moves(position.possible_states());
         if(maximizingplayer){
             int maxEval = std::numeric_limits<int>::min();
-            for(int x = 0; x < position.possible_states().size(); x ++){
-                eval = minimax(position.possible_states()[x], depth - 1,alpha, beta, false);
+            for(auto const& move : orderedMoves){
+                eval = minimax(move, depth - 1,alpha, beta, false);
                 maxEval = std::max(maxEval, eval);
                 alpha = std::max(alpha, eval);
                 if(beta <= alpha){
@@ -35,8 +37,8 @@ namespace minimax{
             return maxEval; 
         }else{
             int minEval = std::numeric_limits<int>::min();
-            for(int x = 0; x < position.possible_states().size(); x ++){
-                eval = minimax(position.possible_states()[x], depth - 1,alpha, beta, false);
+            for(auto const& move : orderedMoves){
+                eval = minimax(move, depth - 1,alpha, beta, false);
                 minEval = std::min(minEval, eval);
                 beta = std::min(beta,eval);
                 if(beta <= alpha){
@@ -45,6 +47,14 @@ namespace minimax{
             }
             return minEval;
         }
+    }
+
+    std::vector<chess::Board> order_moves(std::vector<chess::Board> moves){
+        auto sort_rule_lambda = [] (chess::Board const& b1, chess::Board const& b2) -> bool{
+            return b1.get_fast_eval() > b2.get_fast_eval();
+        };
+        std::sort(moves.begin(), moves.end(), sort_rule_lambda);
+        return moves;
     }
 }
 
