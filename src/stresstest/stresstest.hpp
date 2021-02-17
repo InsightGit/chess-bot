fa076ca2a8bd5cb304d267ce209ddca669928a99
InterestingBrainPoops <satasiyahet@gmail.com>
2021-02-17 08:55:41 -0800

    comments

--- src/stresstest/stresstest.hpp
+++ src/stresstest/stresstest.hpp
@@ -2,6 +2,8 @@
 #define STRESSTEST_HPP
 #include "board.hpp"
 namespace stresstest{
+    // what this *should* do is return all of the possible board states at a given depth. 
+    // This would help a LOT when your trying to test the engine half.
     int getAllMoves(chess::Board board, int depth){
         if(depth == 0 || board.game_end_status() != chess::GameOutcome::Tie){
             return 0;
