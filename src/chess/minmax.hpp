2f541a988cadb3c870aaad828d2c4d1d847ac3b1
InterestingBrainPoops <satasiyahet@gmail.com>
2021-02-16 19:02:41 -0800

    minimax w/ alphabeta pruning

--- src/chess/minmax.hpp
+++ src/chess/minmax.hpp
@@ -6,7 +6,7 @@
 #include <limits>
 namespace minimax{
     
-    int minimax(chess::Board position, int depth,bool maximizingplayer){
+    int minimax(chess::Board position, int depth, int alpha, int beta, bool maximizingplayer){
         if(depth == 0 || position.possible_states().size() == 0){
             return position.getStaticEval();
         }
@@ -14,15 +14,23 @@ namespace minimax{
         if(maximizingplayer){
             int maxEval = std::numeric_limits<int>::min();
             for(int x = 0; x < position.possible_states().size(); x ++){
-                eval = minimax(position.possible_states()[x], depth - 1, false);
+                eval = minimax(position.possible_states()[x], depth - 1,alpha, beta, false);
                 maxEval = std::max(maxEval, eval);
+                alpha = std::max(alpha, eval);
+                if(beta <= alpha){
+                    break;
+                }
             }
-            return maxEval;
+            return maxEval; 
         }else{
             int minEval = std::numeric_limits<int>::min();
             for(int x = 0; x < position.possible_states().size(); x ++){
-                eval = minimax(position.possible_states()[x], depth - 1, false);
+                eval = minimax(position.possible_states()[x], depth - 1,alpha, beta, false);
                 minEval = std::min(minEval, eval);
+                beta = std::min(beta,eval);
+                if(beta <= alpha){
+                    break;
+                }
             }
             return minEval;
         }
