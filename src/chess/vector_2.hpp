815ddda01a4899d86869bf2c590615b93f8fd5a1
Bobby Youstra <bobbyyoustra@protonmail.com>
2021-02-22 11:06:58 -0800

    Improved Vector2 class with composite number support

--- src/chess/vector_2.hpp
+++ src/chess/vector_2.hpp
@@ -5,26 +5,47 @@
 #ifndef CHESS_BOT_VECTOR_2_HPP
 #define CHESS_BOT_VECTOR_2_HPP
 
+#include <cstdint>
+
 namespace chess {
     class Vector2 {
     public:
         Vector2(int x, int y) {
-            m_x = x;
-            m_y = y;
+            // if we want to save some memory at the expense of code simplicity, define this preprocessor definition
+            #ifndef _DISABLE_XY
+                m_x = x;
+                m_y = y;
+            #endif
+
+            m_composite = m_x + (m_y * 8);
         }
 
         Vector2() : Vector2(0, 0) {}
 
-        int get_x() const {
-            return m_x;
+        #ifndef _DISABLE_XY
+            uint8_t get_x() const {
+                return m_x;
+            }
+
+            uint8_t get_y() const {
+                return m_y;
+            }
+        #endif
+
+        uint8_t get_composite() const {
+            return m_composite;
         }
 
-        int get_y() const {
-            return m_y;
+        bool operator==(const Vector2 &other) const {
+            return m_composite == other.m_composite;
         }
     private:
-        int m_x;
-        int m_y;
+        uint8_t m_composite;
+
+        #ifndef _DISABLE_XY
+            uint8_t m_x;
+            uint8_t m_y;
+        #endif
     };
 }
 
